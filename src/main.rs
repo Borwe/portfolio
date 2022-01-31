4bebe5818ad3c9ab7463a8277e67204677e4869f
brian.orwe <brian.orwe@gmail.com>
2022-01-31 22:05:52 +0300

    Can now serve angular files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+use actix_web::{web,HttpRequest, HttpServer, Result, App};
+use actix_files as afs;
+
+async fn files(req: HttpRequest)-> Result<afs::NamedFile>{
+  let path: String = req.match_info().query("filename").parse().unwrap();
+  let mut base_files_dir = String::from("./dist/portfolio/");
+  base_files_dir+=&path[..];
+  Ok(afs::NamedFile::open(base_files_dir.as_str())?)
+}
+
+async fn index()-> Result<afs::NamedFile>{
+  Ok(afs::NamedFile::open("./dist/portfolio/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    let server = HttpServer::new(||{
+      App::new().route("/",web::get().to(index))
+        .route("/{filename:.*}",web::get().to(files))
+    });
+    server.bind("0.0.0.0:8080")?.run().await?;
     println!("Hello, world!");
+    Ok(())
 }
