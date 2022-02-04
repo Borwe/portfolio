db24e6d24638fbb30ef1bb493e8f9b14a81fe833
brian.orwe <brian.orwe@gmail.com>
2022-02-04 19:14:29 +0300

    Now redirects to index, if user tries funny urls

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use actix_web::{web,HttpRequest, HttpServer, Result, App};
 use actix_files as afs;
 use std::path::Path;
 use std::fs;
+use std::sync::Arc;
 
-async fn files(req: HttpRequest)-> Result<afs::NamedFile>{
+async fn files(req: HttpRequest, files: web::Data<Arc<Vec<String>>>)-> Result<afs::NamedFile>{
   let path: String = req.match_info().query("filename").parse().unwrap();
   let mut base_files_dir = String::from("./dist/portfolio/");
-  base_files_dir+=&path[..];
+  if files.contains(&path) == false {
+    base_files_dir.push_str("index.html");
+  }else{
+    base_files_dir+=&path[..];
+  }
   Ok(afs::NamedFile::open(base_files_dir.as_str())?)
 }
 
@@ -15,18 +20,20 @@ async fn index()-> Result<afs::NamedFile>{
 }
 
 fn get_items_in_dir(dir: &Path, items_in_dir: &mut Vec<String>) -> std::io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                get_items_in_dir(&path, items_in_dir)?;
-            } else {
-                items_in_dir.push(path.to_str().unwrap().to_owned());
-            }
-        }
+  if dir.is_dir() {
+    for entry in fs::read_dir(dir)? {
+      let entry = entry?;
+      let path = entry.path();
+      if path.is_dir() {
+        get_items_in_dir(&path, items_in_dir)?;
+      } else {
+        let path = path.to_str().unwrap().to_owned()
+          .strip_prefix("./dist/portfolio/").unwrap().to_owned();
+        items_in_dir.push(path);
+      }
     }
-    Ok(())
+  }
+  Ok(())
 }
 
 #[actix_web::main]
@@ -34,8 +41,11 @@ async fn main() -> std::io::Result<()>{
   let mut items_in_dir = Vec::<String>::new();
   get_items_in_dir(Path::new("./dist/"), &mut items_in_dir)?;
 
-  let server = HttpServer::new(||{
-    App::new().route("/",web::get().to(index))
+  let items_in_dir = Arc::<Vec<String>>::from(items_in_dir);
+
+  let server = HttpServer::new(move ||{
+    App::new().data(Arc::clone(&items_in_dir))
+      .route("/",web::get().to(index))
       .route("/{filename:.*}",web::get().to(files))
   }).bind("0.0.0.0:8080")?.run();
   server.await?;
