0ee7ffbd1c044b680cbefc9d0c4792896de3b955
brian.orwe <brian.orwe@gmail.com>
2022-02-04 15:42:06 +0300

    Can now get files in dist folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{web,HttpRequest, HttpServer, Result, App};
 use actix_files as afs;
+use std::path::Path;
+use std::fs;
 
 async fn files(req: HttpRequest)-> Result<afs::NamedFile>{
   let path: String = req.match_info().query("filename").parse().unwrap();
@@ -12,13 +14,31 @@ async fn index()-> Result<afs::NamedFile>{
   Ok(afs::NamedFile::open("./dist/portfolio/index.html")?)
 }
 
+fn get_items_in_dir(dir: &Path, items_in_dir: &mut Vec<String>) -> std::io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                get_items_in_dir(&path, items_in_dir)?;
+            } else {
+                items_in_dir.push(path.to_str().unwrap().to_owned());
+            }
+        }
+    }
+    Ok(())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    let server = HttpServer::new(||{
-      App::new().route("/",web::get().to(index))
-        .route("/{filename:.*}",web::get().to(files))
-    });
-    server.bind("0.0.0.0:8080")?.run().await?;
-    println!("Hello, world!");
-    Ok(())
+  let mut items_in_dir = Vec::<String>::new();
+  get_items_in_dir(Path::new("./dist/"), &mut items_in_dir)?;
+
+  let server = HttpServer::new(||{
+    App::new().route("/",web::get().to(index))
+      .route("/{filename:.*}",web::get().to(files))
+  }).bind("0.0.0.0:8080")?.run();
+  server.await?;
+  println!("Hello, world!");
+  Ok(())
 }
