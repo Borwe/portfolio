0a6324466dd75e76612d801b7cf8bb971888f2b3
brian.orwe <brian.orwe@gmail.com>
2022-02-05 15:59:07 +0300

    Create docker-compose for postgres test server

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use actix_files as afs;
 use std::path::Path;
 use std::fs;
 use std::sync::Arc;
+use futures::future;
 
 async fn files(req: HttpRequest, files: web::Data<Arc<Vec<String>>>)-> Result<afs::NamedFile>{
   let path: String = req.match_info().query("filename").parse().unwrap();
@@ -36,19 +37,39 @@ fn get_items_in_dir(dir: &Path, items_in_dir: &mut Vec<String>) -> std::io::Resu
   Ok(())
 }
 
+async fn get_data_from_github() -> std::io::Result<()>{
+  std::thread::sleep(std::time::Duration::from_secs(40));
+  println!("Hello, world!");
+  Ok(())
+}
+
+async fn start_db_filling() -> std::io::Result<()>{
+  Ok(())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
+  // get items that can be served to the db
   let mut items_in_dir = Vec::<String>::new();
   get_items_in_dir(Path::new("./dist/"), &mut items_in_dir)?;
-
   let items_in_dir = Arc::<Vec<String>>::from(items_in_dir);
+
+  // check if db pr's or projects is empty.
+  // if yes, then get the data to fill it up with
+  // if not, then proceed to continue scrapping if there
+  // is new data.
+
+  let get_data_fut = get_data_from_github();
 
   let server = HttpServer::new(move ||{
     App::new().data(Arc::clone(&items_in_dir))
       .route("/",web::get().to(index))
       .route("/{filename:.*}",web::get().to(files))
   }).bind("0.0.0.0:8080")?.run();
-  server.await?;
-  println!("Hello, world!");
+
+  let output = future::join(get_data_fut,server);
+  let (get_data_result, server_result) = output.await;
+  get_data_result?;
+  server_result?;
   Ok(())
 }
