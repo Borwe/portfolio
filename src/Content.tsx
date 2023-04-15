01198eab748d071ce29f11bd9cd04e7875b12b47
brian.orwe <brian.orwe@gmail.com>
2023-04-15 23:08:36 +0300

    Right head animates with gsap

--- src/Content.tsx
+++ src/Content.tsx
@@ -3,19 +3,11 @@ import { Box } from "@mui/material";
 import Right from "./content/Right";
 import "./content/Content.css";
 import About from "./content/About";
-import Grid from "@mui/material/Unstable_Grid2";
 
 const Content: FC = ()=>{
   return (<>
     <Right/>
-    {/*Used to store the pages, with a 7:5 screen covered*/}
-    <Grid container mb="30px">
-      <Grid xs={6}>
-        <Box id="left"><About /></Box>
-      </Grid>
-      <Grid xs={6}>
-      </Grid>
-    </Grid>
+    <Box id="left"><About /></Box>
     </>
   );
 } 
--- src/content/Right.tsx
+++ src/content/Right.tsx
@@ -1,9 +1,10 @@
 import { Box } from "@mui/material";
-import { FC, MouseEvent } from "react";
+import { FC, MouseEvent, useEffect, useRef } from "react";
 import "./Right.css";
 import right_img from "../imgs/look_left_up_black_border.png";
 import right_black_red from "../imgs/look_left_up_red.png";
 import right_black_green from "../imgs/look_left_up_green.png";
+import gsap from "gsap";
 
 type Position = {
 	x: number,
@@ -17,17 +18,41 @@ type ImagePositions = {
 };
 
 const Right: FC = () => {
-	const rightHover = (event: MouseEvent) => {
-		console.log("X:", event.screenX, "Y:", event.screenY);
-	}
-	return <Box id="right" onMouseMove={rightHover} >
-		<Box id="right_img_top" >
+	const top = useRef(null);
+	const red = useRef(null);
+	const green = useRef(null);
+
+	useEffect(()=>{
+		gsap.to(top.current, {
+			bottom: "5%",
+			duration: 1,
+			repeat: -1,
+			yoyo: true
+		})
+		gsap.to(red.current, {
+			bottom: "5%",
+			repeat: -1,
+			duration: 1,
+			delay: 0.1,
+			yoyo: true
+		});
+		gsap.to(green.current, {
+			bottom: "5%",
+			repeat: -1,
+			duration: 1,
+			delay: 0.2,
+			yoyo: true
+		});
+	}, [top,green, red]);
+
+	return <Box id="right" >
+		<Box ref={top} id="right_img_top" >
 			<img src={right_img} height="75%" alt="My image" />
 		</Box>
-		<Box id="right_img_red_mid">
+		<Box ref={red} id="right_img_red_mid">
 			<img src={right_black_red} height="75%" alt="My image" />
 		</Box>
-		<Box id="right_img_green">
+		<Box ref={green} id="right_img_green">
 			<img src={right_black_green} height="75%" alt="My image" />
 		</Box>
 	</Box>
