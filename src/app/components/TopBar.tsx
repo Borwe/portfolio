3a05b5e90573ced3f4cf80de9b7b0e0832c944b5
brian.orwe <brian.orwe@gmail.com>
2023-06-26 00:50:03 +0300

    Right flag seems to work on small screen too

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -102,7 +102,7 @@ const FlagBackground: FC<{
 
 	return <Box width={"100%"} height={"100%"} sx={{
 		position: "absolute",
-		display: "box",
+		display: "block",
 		top: "0px",
 		left: "0px",
 	}}>
--- src/app/components/content/Right.tsx
+++ src/app/components/content/Right.tsx
@@ -35,22 +35,61 @@ function setupGsapForRightLeftSplit(top: ElementRef,
 		})];
 }
 
-const RightFlag: FC = ()=>{
+const RightFlag: FC<{isHalf: boolean}> = (props)=>{
+	console.log("HALF: ",props.isHalf)
 	const black = useRef(HTMLDivElement.prototype);
 	const white1 = useRef(HTMLDivElement.prototype);
 	const red = useRef(HTMLDivElement.prototype);
 	const white2 = useRef(HTMLDivElement.prototype);
 
+
 	// TODO animate flags to down 
+	useEffect(()=>{
+		gsap.to(black.current,{
+			height: (props.isHalf ? window.innerHeight :
+				window.innerHeight * 0.25),
+			duration: 1,
+			delay: 5
+		})
+		gsap.to(white1.current,{
+			height: (props.isHalf ? window.innerHeight :
+				window.innerHeight * 0.25),
+			duration: 1,
+			delay: 4
+		})
+		//gsap.to(red.current,{
+		//	height: (props.isHalf ? window.innerHeight - 30 :
+		//		window.innerHeight * 0.25+"px"),
+		//	duration: 1,
+		//	delay: 3
+		//})
+		//gsap.to(white2.current,{
+		//	height: (props.isHalf ? window.innerHeight - 30 :
+		//		window.innerHeight * 0.25+"px"),
+		//	duration: 1,
+		//	delay: 2
+		//})
+	}, [black, white1, red, white2, props.isHalf]);
 	// TODO Handle opening the divs like a card scrolling 
 	// according to section
 
-	return (<>
-		<Box typeof="div" ref={black} />
-		<Box typeof="div" ref={white1} />
-		<Box typeof="div" ref={red} />
-		<Box typeof="div" ref={white2} />
-		</>)
+	return (
+		<Box sx={{width: "100%",
+				position: "fixed",
+				height: (props.isHalf ? window.innerHeight :
+				window.innerHeight * 0.25),
+				zIndex: 95}}>
+			<Box id="black_flag"  ref={black} />
+			<Box id="white1_flag" ref={white1} />
+			<Box sx={{zIndex: 104,
+					backgroundColor: "red",
+					top: "0px"
+			}} ref={red} />
+			<Box sx={{zIndex: 102,
+					backgroundColor: "white",
+					top: "0px"
+			}} ref={white2} />
+		</Box>)
 }
 
 const Right: FC<{ isHalf: boolean }> = (props) => {
@@ -66,10 +105,8 @@ const Right: FC<{ isHalf: boolean }> = (props) => {
 	}, [props.isHalf, window.innerWidth]);
 
 	return <>
-		<Box sx={{ zIndex: 99}}>
-			<RightFlag/>
-		</Box>
-		<Box sx={{ width: "100%", zIndex: 100 }}>
+		<RightFlag isHalf={props.isHalf}/>
+		<Box sx={{ width: "100%", zIndex: 111 }}>
 			<img ref={top} id="right_img_top" src={right_img} alt="My image" />
 			<img ref={red} id="right_img_red_mid"
 				src={right_black_red} alt="My image" />
