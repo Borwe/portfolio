f63ee87eb79ddbb4273822291c53f89e3070240f
brian.orwe <brian.orwe@gmail.com>
2026-04-28 17:48:44 +0300

    Server starts

---
+++ docs/index.md
@@ -0,0 +1,1 @@
+# Welcome, I am Brian Orwe.
--- portgen/src/main.rs
+++ portgen/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::Html, routing::get};
+
+async fn index()-> Html<&'static str> {
+    Html("<h3>YOLO</h3>")
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(index));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
