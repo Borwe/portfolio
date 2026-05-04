aa785ef288861f7e506d60f6a67671452c59c874
brian.orwe <brian.orwe@gmail.com>
2026-05-05 00:23:32 +0300

    Parse for file changes in a looped async task

--- portgen/src/main.rs
+++ portgen/src/main.rs
@@ -1,7 +1,7 @@
-use std::{cell::RefCell, env::current_dir, ffi::OsStr, fs::{self, DirEntry, File, ReadDir}, io, iter::{self, FilterMap}, path::PathBuf, rc::Rc, time::SystemTime};
+use std::{alloc::{Layout, alloc}, cell::RefCell, env::current_dir, ffi::OsStr, fs::{self, DirEntry, File, ReadDir}, io, iter::{self, FilterMap}, path::PathBuf, rc::Rc, sync::Arc, time::{Duration, SystemTime}, usize};
 
 use axum::{Router, response::Html, routing::get};
-use sha256::try_digest;
+use tokio::{spawn, time::sleep};
 
 async fn index()-> Html<&'static str> {
     Html("<h3>YOLO</h3>")
@@ -9,9 +9,10 @@ async fn index()-> Html<&'static str> {
 
 struct State{
     files: Vec<PathBuf>,
-    files_pev: Option<Vec<PathBuf>>,
+    files_pev: Vec<PathBuf>,
     files_dates: Vec<SystemTime>,
-    files_prev_dates: Vec<SystemTime>
+    files_prev_dates: Vec<SystemTime>,
+    files_change_index: Vec<usize>
 }
 
 impl State {
@@ -24,12 +25,12 @@ impl State {
 
         while !other_dirs.is_empty() {
             if let Some(path_dir) = other_dirs.pop() {
-                println!("DIR {}", path_dir.to_str().unwrap());
                 paths.append(&mut Self::read_dirs(path_dir, &mut other_dirs));
             }
         }
 
         self.files = paths;
+        self.files.sort();
         self.produce_hashes();
     }
 
@@ -71,27 +72,78 @@ impl State {
 
 impl Default for State {
     fn default() -> Self {
-        println!("HMMMMMM");
         let mut state = Self {
             files: Default::default(),
             files_dates: Default::default(),
             files_prev_dates: Default::default(),
-            files_pev: Default::default()
+            files_pev: Default::default(),
+            files_change_index: Default::default()
         };
         state.grep_files();
-        for f in state.files.iter() {
-            println!("{}",f.as_os_str().to_string_lossy());
-        }
-        println!("DONE!!!");
         state
     }
 
 }
 
+async fn interval_pinging_changes(state: &mut State){
+    loop {
+        let start = SystemTime::now();
+        {
+            state.files_pev.clear();
+            state.files_prev_dates.clear();
+            state.files_change_index.clear();
+        }
+
+        state.files_pev.append(&mut state.files);
+        state.files_prev_dates.append(&mut state.files_dates);
+        println!("YOLO");
+
+        state.grep_files();
+
+        {
+            state.files_change_index.clear();
+            state.files_change_index.reserve(state.files.len());
+        }
+
+        println!("LEN_BEF {}",state.files_pev.len());
+        println!("LEN_NOW {}",state.files.len());
+
+        let mut used = 0;
+        for (i, v_prev) in state.files_pev.iter().enumerate() {
+            for(j, v) in state.files[used..].iter().enumerate() {
+                //println!("used: {:?}",used);
+                if v==v_prev{
+                    used = j;
+                    if state.files_prev_dates[i] < state.files_dates[j] {
+                        state.files_change_index.push(j)
+                    }
+                    break
+                }
+            }
+        }
+
+
+        let end = SystemTime::now();
+        let secs = end.duration_since(start).unwrap().as_secs();
+        println!("TIME PASSED: {secs}");
+    }
+}
+
+fn get_mut_ref<'a,T>(ptr: *mut T)-> &'a mut T{
+    unsafe {
+        ptr.as_mut::<'a>().unwrap()
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let mut state = State::default();
+    let state = unsafe { alloc(Layout::new::<State>()) as *mut State };
+    spawn({
+        let state = get_mut_ref(state);
+        async move {
+            interval_pinging_changes(state).await
+        }
+    });
 
     let router = Router::new().route("/", get(index));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
