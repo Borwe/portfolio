939fa5ed27b66e3af9e69cd29f97765203e088f9
brian.orwe <brian.orwe@gmail.com>
2026-06-16 19:18:42 +0300

    Add libuv and bo_meson

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include <stdlib.h>
+#define BO_ARENA_IMPLEMENTATION
+#include <bo_arena.h>
+
+#define ARENA_SIZE 1024*1024*20 
+
+static void free_arena(bo_arena *arena){
+}
 
 int main(int argc, char **argv){
-    printf("Portgen project\n");
+    char *buff = malloc(ARENA_SIZE);
+    bo_arena arena = bo_make_arena(buff, ARENA_SIZE, true, free_arena);
     return 0;
 }
