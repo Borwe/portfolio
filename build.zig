e961f99bd1a8e7dd08dd36684ea324f82c7241d4
brian.orwe <brian.orwe@gmail.com>
2026-06-22 15:52:28 +0300

    fix: Cleanup

--- build.zig
+++ build.zig
@@ -4,20 +4,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("portfolio", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
-
     const exe = b.addExecutable(.{
         .name = "portfolio",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
-            .imports = &.{
-                .{ .name = "portfolio", .module = mod },
-            },
         }),
     });
 
@@ -34,11 +26,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
 
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
@@ -47,6 +34,5 @@ pub fn build(b: *std.Build) void {
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
