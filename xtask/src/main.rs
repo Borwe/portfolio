0e4e887e0f511aef201917f3a0a5341b16f74ef7
brian.orwe <brian.orwe@gmail.com>
2026-07-04 16:45:30 +0300

    chore: Update xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    error::Error,
+    process::{exit, Command},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Pass an argument please");
+        exit(-1);
+    }
+
+    if args[1] == "portgen" || args[1] == "port" {
+        let mut child = Command::new("cargo")
+            .args(["run", "-p", "portgen"])
+            .spawn()?;
+        let status = child.wait()?;
+        exit(status.code().unwrap());
+    }
+
+    Ok(())
 }
